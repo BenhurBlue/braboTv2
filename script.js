73ba36892d50a7ec824d8971e53220f3b1ed7ee0
Benhurblue <benhur.black.f@gmail.com>
2022-09-18 14:07:47 -0300

    corinthians é o maior time do mundo

---
+++ script.js
@@ -0,0 +1,11 @@
+let pessoa = {
+    nome: 'Benhur',
+    idade: 20,
+    time: 'Corinthians',
+    Beleza: 'mt lindo',
+    altura: '1,74cm',
+    Vagabundo: true
+
+};
+
+console.log(pessoa['nome']);
---
+++ time.js
@@ -0,0 +1,66 @@
+let time = [
+    {
+        nome: 'cassio',
+        camisa: 12,
+        posição:'Goleiro'
+    },
+    {
+        nome: 'Fabio Santos',
+        camisa: 26,
+        posição:'Lateral Esquerdo'
+    },
+
+    {
+        nome: 'Balbuena',
+        camisa: 31,
+        posição:'Zagueiro'
+    },
+    {
+        nome: 'Bruno Mendez',
+        camisa: 25,
+        posição:'Zagueiro'
+    },
+    {
+        nome: 'Fagner',
+        camisa: 23,
+        posição:'Lateral Direito'
+    },
+    {
+        nome: 'Fauto Vera',
+        camisa: 33,
+        posição:'Volante'
+    },
+    {
+        nome: 'Dú queiroz',
+        camisa: 37,
+        posição:'Volante'
+    },
+    {
+        nome: 'Renato Augusto',
+        camisa: 8,
+        posição:'Meio Campo'
+    },
+    {
+        nome: 'Mosquito',
+        camisa: 19,
+        posição:'Ponta Direita'
+    },
+    {
+        nome: 'Yuri Alberto',
+        camisa: 9,
+        posição:'Centro Avante'
+    },
+    {
+        nome: 'Roger Guedes',
+        camisa: 10,
+        posição:'Ponta Esquerda'
+    },
+    {
+        nome: 'Vitor Perereira',
+        camisa:null ,
+        posição:'Tecnico'
+    }
+]
+
+console.log(time.length);
+console.table(time);
